8fb191d7533a19590556dd43c31962bc0a1a00c3
Viktor Kartashov <viktor@scribesecurity.com>
2024-07-18 17:30:17 +0300

    init

---
+++ first.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	fmt.Println("Opening the DB")
+	db, err := sql.Open("sqlite3", "file:./test.db?_locking=EXCLUSIVE&_mode=rwc&_mutex=full&_busy_timeout=60000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create table if not exists
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY AUTOINCREMENT, value TEXT)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	iterations := 10000
+
+	// Write operations
+	for i := 0; i < iterations; i++ {
+		value := fmt.Sprintf("Write %d", i)
+		_, err := db.Exec(`INSERT INTO test (value) VALUES (?)`, value)
+		if err != nil {
+			log.Fatalf("Error inserting value %s: %v\n", value, err)
+		}
+		fmt.Printf("Writer: Inserted %s\n", value)
+	}
+	db.Close()
+
+	// Read operation
+	time.Sleep(1 * time.Second)
+	fmt.Println("Reader: Reading from DB:")
+	db, err = sql.Open("sqlite3", "file:./test.db?_mode=ro&_mutex=full&_busy_timeout=60000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Opened the DB")
+	defer db.Close()
+
+	file, err := os.OpenFile("result.first.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	writer := io.Writer(file)
+
+	fmt.Println("Running SELECT query")
+	rows, err := db.Query(`SELECT id, value FROM test`)
+	if err != nil {
+		log.Fatalf("Error reading data: %v\n", err)
+	}
+	fmt.Println("Got response from SELECT query")
+
+	for rows.Next() {
+		var id int
+		var value string
+		err = rows.Scan(&id, &value)
+		if err != nil {
+			log.Fatalf("Error scanning row: %v\n", err)
+		}
+		data := fmt.Sprintf("Reader: id: %d, value: %s\n", id, value)
+		_, err := writer.Write([]byte(data))
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatalf("Error iterating rows: %v\n", err)
+	}
+}
---
+++ verify.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	filePath := "result.txt"
+	if len(os.Args) > 1 {
+		fmt.Println("Using file:", os.Args[1])
+		filePath = os.Args[1]
+	}
+	iterations := 10000
+	expectedWrites := makeExpectedValues("Write", iterations)
+	expectedReads := makeExpectedValues("Read", iterations)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		matchesExpected(line, &expectedWrites)
+		matchesExpected(line, &expectedReads)
+		// if matchesExpected(line, &expectedWrites) {
+		// 	// fmt.Println("Found Write:", line)
+		// 	expectedWrites[line] = true
+		// } else if matchesExpected(line, &expectedReads) {
+		// 	// fmt.Println("Found Read:", line)
+		// 	expectedReads[line] = true
+		// }
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+
+	// Check if all expected writes and reads were found
+	if !allFound(expectedWrites) {
+		log.Println("Not all Write values found")
+	} else {
+		fmt.Println("All Write values found")
+	}
+	if !allFound(expectedReads) {
+		log.Println("Not all Read values found")
+	} else {
+		fmt.Println("All Read values found")
+	}
+}
+
+// makeExpectedValues creates a map of expected values for a given prefix and count.
+func makeExpectedValues(prefix string, count int) map[string]bool {
+	expected := make(map[string]bool)
+	for i := 0; i < count; i++ {
+		expected[fmt.Sprintf("%s %d", prefix, i)] = false
+	}
+	return expected
+}
+
+// matchesExpected checks if the line matches any expected value in the map.
+func matchesExpected(line string, expected *map[string]bool) bool {
+	if line == "" {
+		return true
+	}
+	for expectedVal := range *expected {
+		if strings.HasSuffix(line, expectedVal) {
+			(*expected)[expectedVal] = true
+			return true
+		}
+	}
+	return false
+}
+
+// allFound checks if all expected values in the map have been marked as found.
+func allFound(expected map[string]bool) bool {
+	for val, found := range expected {
+		if !found {
+			fmt.Println("Expected value not found:", val)
+			return false
+		}
+	}
+	return true
+}
